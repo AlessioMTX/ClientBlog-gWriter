ba9d84116d4498688af6e0fdaa1340c8b6be71f1
Patryk Rzucidlo <ptkdev@gmail.com>
2011-04-05 11:32:15 +0200

    New compile options, rebased all .c files

--- src/gwriter.c
+++ src/gwriter.c
@@ -1,230 +1,31 @@
+/* 
+*	gWriterBlog - Blog Editor For Linux Desktop
+*		Copyright (C) 2011  PTKDev
+*
+*		This program is free software: you can redistribute it and/or modify
+*		it under the terms of the GNU General Public License as published by
+*		the Free Software Foundation, either version 3 of the License, or
+*		(at your option) any later version.
+*
+*		This program is distributed in the hope that it will be useful,
+*		but WITHOUT ANY WARRANTY; without even the implied warranty of
+*		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*		GNU General Public License for more details.
+*
+*		You should have received a copy of the GNU General Public License
+*		along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*		
+*			
+*		Author: Patryk Rzucidlo (PTKDev)
+*		Twitter: @ptkdev / @gwriterblog_en
+*		WebSite: http://www.gwriterblog.org
+*/
+
+
 #include <gtk/gtk.h>
 #include <glib.h>
 #include <stdlib.h>
-
-void windows_about()
-{
-  GdkPixbuf* pixbuf = gdk_pixbuf_new_from_file ("../img/gwb_about.png", NULL);
-  GtkWidget* dialog = gtk_about_dialog_new();
-
-  GError* error = NULL;
-
-  gtk_window_set_icon_from_file (GTK_WINDOW (dialog), "../img/favicon.png", &error);
-
-  gtk_about_dialog_set_name (GTK_ABOUT_DIALOG (dialog), "gWriter Blog");
-  gtk_about_dialog_set_version (GTK_ABOUT_DIALOG (dialog), "");
-  gtk_about_dialog_set_copyright (GTK_ABOUT_DIALOG (dialog), "(c) PTKDev");
-  gtk_about_dialog_set_comments (GTK_ABOUT_DIALOG (dialog), "Basato su librerie GTK e semplicità!\n\nVersion: 0.1.1");
-  gtk_about_dialog_set_website (GTK_ABOUT_DIALOG (dialog), "http://www.gwriterblog.org/");
-
-  gtk_about_dialog_set_logo (GTK_ABOUT_DIALOG (dialog), pixbuf);
-  g_object_unref (pixbuf), pixbuf = NULL;
-
-  gtk_dialog_run (GTK_DIALOG (dialog) );
-  gtk_widget_destroy (dialog);
-
-}
-
-static void switch_page (GtkButton* button, GtkNotebook* notebook)
-{
-  if (gtk_notebook_get_current_page (notebook) == 0) {
-    gtk_notebook_set_current_page (notebook, 1);
-  } else {
-    gtk_notebook_set_current_page (notebook, 0);
-  }
-}
-
-// Add User
-void windows_adduser()
-{
-  //Inizializzazioni
-  GtkWidget* window;
-  GtkWidget* table;
-  GtkWidget* label;
-  GtkWidget* entry_api;
-  GtkWidget* button;
-
-  GError* error = NULL;
-
-  // Dichiaro finestra
-  window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-  gtk_window_set_default_size (GTK_WINDOW (window), 310, 350);
-  gtk_widget_set_size_request (window, 310, 350);
-  gtk_window_set_title (GTK_WINDOW (window), "New Blog");
-  gtk_container_set_border_width (GTK_CONTAINER (window), 0);
-  gtk_window_set_position (GTK_WINDOW (window), GTK_WIN_POS_CENTER);
-
-  // Dichiaro finestra: favicon
-  gtk_window_set_icon_from_file (GTK_WINDOW (window), "../img/favicon.png", &error);
-
-  // Dichiaro autenticazione
-  table = gtk_table_new (11, 10, TRUE);
-  gtk_container_add (GTK_CONTAINER (window), table);
-
-  label = gtk_label_new ("Provider");
-  gtk_label_set_justify (GTK_LABEL (label), GTK_JUSTIFY_LEFT);
-  GList* items_blog = NULL;
-  items_blog = g_list_append (items_blog, "Wordpress");
-  entry_api = gtk_combo_new ();
-  gtk_combo_set_popdown_strings (GTK_COMBO (entry_api), items_blog);
-
-  gtk_table_attach (GTK_TABLE (table), label, 1, 9,
-                    0, 1, GTK_FILL | GTK_EXPAND,
-                    GTK_FILL | GTK_EXPAND, 0, 0);
-  gtk_table_attach (GTK_TABLE (table), entry_api, 1, 9,
-                    1, 2, GTK_FILL | GTK_EXPAND,
-                    GTK_FILL | GTK_EXPAND, 0, 0);
-
-  label = gtk_label_new ("Url Blog:");
-  gtk_label_set_justify (GTK_LABEL (label), GTK_JUSTIFY_LEFT);
-  entry_api = gtk_entry_new ();
-  gtk_entry_set_text (GTK_ENTRY (entry_api), "");
-
-  gtk_table_attach (GTK_TABLE (table), label, 1, 9,
-                    2, 3, GTK_FILL | GTK_EXPAND,
-                    GTK_FILL | GTK_EXPAND, 0, 0);
-  gtk_table_attach (GTK_TABLE (table), entry_api, 1, 9,
-                    3, 4, GTK_FILL | GTK_EXPAND,
-                    GTK_FILL | GTK_EXPAND, 0, 0);
-
-  label = gtk_label_new ("NickName:");
-  gtk_label_set_justify (GTK_LABEL (label), GTK_JUSTIFY_LEFT);
-  entry_api = gtk_entry_new ();
-  gtk_entry_set_text (GTK_ENTRY (entry_api), "");
-
-  gtk_table_attach (GTK_TABLE (table), label, 1, 9,
-                    4, 5, GTK_FILL | GTK_EXPAND,
-                    GTK_FILL | GTK_EXPAND, 0, 0);
-  gtk_table_attach (GTK_TABLE (table), entry_api, 1, 9,
-                    5, 6, GTK_FILL | GTK_EXPAND,
-                    GTK_FILL | GTK_EXPAND, 0, 0);
-
-  label = gtk_label_new ("Password:");
-  gtk_label_set_justify (GTK_LABEL (label), GTK_JUSTIFY_LEFT);
-  entry_api = gtk_entry_new ();
-  gtk_entry_set_text (GTK_ENTRY (entry_api), "");
-
-  gtk_table_attach (GTK_TABLE (table), label, 1, 9,
-                    6, 7, GTK_FILL | GTK_EXPAND,
-                    GTK_FILL | GTK_EXPAND, 0, 0);
-  gtk_table_attach (GTK_TABLE (table), entry_api, 1, 9,
-                    7, 8, GTK_FILL | GTK_EXPAND,
-                    GTK_FILL | GTK_EXPAND, 0, 0);
-
-
-  button = gtk_button_new_with_label ("SALVA");
-  gtk_table_attach (GTK_TABLE (table), button, 1, 9,
-                    9, 10, GTK_FILL | GTK_EXPAND,
-                    GTK_FILL | GTK_EXPAND, 0, 0);
-
-  gtk_container_add (GTK_CONTAINER (window), table);
-
-  //Eventi Di Chiusura
-  g_signal_connect (G_OBJECT (window), "delete_event", FALSE, NULL);
-  gtk_widget_show_all (window);
-
-}
-
-// Setting
-void windows_setting()
-{
-
-  GtkWidget* window;
-  GtkWidget* notebook;
-  GtkWidget* setting_menu;
-  GtkWidget* table;
-  GtkWidget* label;
-  GtkWidget* entry_nick;
-  GtkWidget* button;
-
-  GError* error = NULL;
-
-  window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-  gtk_window_set_default_size (GTK_WINDOW (window), 310, 300);
-  gtk_widget_set_size_request (window, 310, 300);
-  gtk_window_set_title (GTK_WINDOW (window), "Setting");
-  gtk_container_set_border_width (GTK_CONTAINER (window), 0);
-  gtk_window_set_position (GTK_WINDOW (window), GTK_WIN_POS_CENTER);
-
-
-  gtk_window_set_icon_from_file (GTK_WINDOW (window), "../img/favicon.png", &error);
-
-  notebook = gtk_notebook_new ();
-  gtk_notebook_set_tab_pos (GTK_NOTEBOOK (notebook), GTK_POS_BOTTOM);
-
-  setting_menu = gtk_label_new ("Account");
-
-  table = gtk_table_new (7, 10, TRUE);
-  label = gtk_label_new ("Blog Da Usare:");
-  gtk_label_set_justify (GTK_LABEL (label), GTK_JUSTIFY_LEFT);
-  GList* items_account = NULL;
-  items_account = g_list_append (items_account, "http://www.sito1.it");
-  items_account = g_list_append (items_account, "http://www.sito2.it");
-  items_account = g_list_append (items_account, "http://www.sito3.it");
-  entry_nick = gtk_combo_new ();
-  gtk_combo_set_popdown_strings (GTK_COMBO (entry_nick), items_account);
-
-  gtk_table_attach (GTK_TABLE (table), label, 1, 9,
-                    0, 1, GTK_FILL | GTK_EXPAND,
-                    GTK_FILL | GTK_EXPAND, 0, 0);
-  gtk_table_attach (GTK_TABLE (table), entry_nick, 1, 9,
-                    1, 2, GTK_FILL | GTK_EXPAND,
-                    GTK_FILL | GTK_EXPAND, 0, 0);
-
-  button = gtk_button_new_with_label ("SALVA");
-  gtk_table_attach (GTK_TABLE (table), button, 1, 9,
-                    5, 6, GTK_FILL | GTK_EXPAND,
-                    GTK_FILL | GTK_EXPAND, 0, 0);
-
-  g_signal_connect (G_OBJECT (table), "clicked", G_CALLBACK (switch_page), notebook);
-  gtk_notebook_append_page (GTK_NOTEBOOK (notebook), table, setting_menu);
-
-
-  setting_menu = gtk_label_new ("Aspetto");
-
-  table = gtk_table_new (7, 10, TRUE);
-  label = gtk_label_new ("Tema Da Usare:");
-  gtk_label_set_justify (GTK_LABEL (label), GTK_JUSTIFY_LEFT);
-  GList* items_skin = NULL;
-  items_skin = g_list_append (items_skin, "default");
-  entry_nick = gtk_combo_new ();
-  gtk_combo_set_popdown_strings (GTK_COMBO (entry_nick), items_skin);
-
-  gtk_table_attach (GTK_TABLE (table), label, 1, 9,
-                    0, 1, GTK_FILL | GTK_EXPAND,
-                    GTK_FILL | GTK_EXPAND, 0, 0);
-  gtk_table_attach (GTK_TABLE (table), entry_nick, 1, 9,
-                    1, 2, GTK_FILL | GTK_EXPAND,
-                    GTK_FILL | GTK_EXPAND, 0, 0);
-
-  label = gtk_label_new ("Lingua Da Usare:");
-  gtk_label_set_justify (GTK_LABEL (label), GTK_JUSTIFY_LEFT);
-  GList* items_lang = NULL;
-  items_lang = g_list_append (items_lang, "italian");
-  entry_nick = gtk_combo_new ();
-  gtk_combo_set_popdown_strings (GTK_COMBO (entry_nick), items_lang);
-
-  gtk_table_attach (GTK_TABLE (table), label, 1, 9,
-                    2, 3, GTK_FILL | GTK_EXPAND,
-                    GTK_FILL | GTK_EXPAND, 0, 0);
-  gtk_table_attach (GTK_TABLE (table), entry_nick, 1, 9,
-                    3, 4, GTK_FILL | GTK_EXPAND,
-                    GTK_FILL | GTK_EXPAND, 0, 0);
-
-  button = gtk_button_new_with_label ("SALVA");
-  gtk_table_attach (GTK_TABLE (table), button, 1, 9,
-                    5, 6, GTK_FILL | GTK_EXPAND,
-                    GTK_FILL | GTK_EXPAND, 0, 0);
-
-  g_signal_connect (G_OBJECT (table), "clicked", G_CALLBACK (switch_page), notebook);
-  gtk_notebook_append_page (GTK_NOTEBOOK (notebook), table, setting_menu);
-
-  gtk_container_add (GTK_CONTAINER (window), notebook);
-
-  g_signal_connect (G_OBJECT (window), "delete_event", FALSE, NULL);
-  gtk_widget_show_all (window);
-
-}
+#include "gwriter.h"
 
 int main (int argc, char* argv[])
 {
@@ -259,7 +60,7 @@ int main (int argc, char* argv[])
   gtk_container_set_border_width (GTK_CONTAINER (window), 0);
   gtk_window_set_position (GTK_WINDOW (window), GTK_WIN_POS_CENTER);
 
-  gtk_window_set_icon_from_file (GTK_WINDOW (window), "../img/favicon.png", &error);
+  gtk_window_set_icon_from_file (GTK_WINDOW (window), "/usr/share/icons/gwriterblog/favicon.png", &error);
 
   g_signal_connect (G_OBJECT (window), "delete_event",
                     G_CALLBACK (gtk_main_quit), NULL);
@@ -273,21 +74,21 @@ int main (int argc, char* argv[])
    * gconftool-2 --type boolean --set /desktop/gnome/interface/menus_have_icons true
    *  */
   file_menu_items = gtk_image_menu_item_new_with_label ("New Blog");
-  icon_menu = gtk_image_new_from_file ("../img/add_user.png");
+  icon_menu = gtk_image_new_from_file ("/usr/share/icons/gwriterblog/add_user.png");
 
   gtk_image_menu_item_set_image (GTK_IMAGE_MENU_ITEM (file_menu_items), icon_menu);
   g_signal_connect (G_OBJECT (file_menu_items), "activate", G_CALLBACK (windows_adduser), NULL);
   gtk_menu_append (GTK_MENU (file_menu_obj), file_menu_items);
 
   file_menu_items = gtk_image_menu_item_new_with_label ("Setting");
-  icon_menu = gtk_image_new_from_file ("../img/setting.png");
+  icon_menu = gtk_image_new_from_file ("/usr/share/icons/gwriterblog/setting.png");
 
   gtk_image_menu_item_set_image (GTK_IMAGE_MENU_ITEM (file_menu_items), icon_menu);
   g_signal_connect (G_OBJECT (file_menu_items), "activate", G_CALLBACK (windows_setting), NULL);
   gtk_menu_append (GTK_MENU (file_menu_obj), file_menu_items);
 
   file_menu_items = gtk_image_menu_item_new_with_label ("Exit");
-  icon_menu = gtk_image_new_from_file ("../img/close.png");
+  icon_menu = gtk_image_new_from_file ("/usr/share/icons/gwriterblog/close.png");
 
   gtk_image_menu_item_set_image (GTK_IMAGE_MENU_ITEM (file_menu_items), icon_menu);
   g_signal_connect (G_OBJECT (file_menu_items), "activate", G_CALLBACK (gtk_main_quit), NULL);
@@ -298,7 +99,7 @@ int main (int argc, char* argv[])
 
 
   aiuto_menu_items = gtk_image_menu_item_new_with_label ("Information");
-  icon_menu = gtk_image_new_from_file ("../img/star.png");
+  icon_menu = gtk_image_new_from_file ("/usr/share/icons/gwriterblog/star.png");
 
   gtk_image_menu_item_set_image (GTK_IMAGE_MENU_ITEM (aiuto_menu_items), icon_menu);
   g_signal_connect (G_OBJECT (aiuto_menu_items), "activate", G_CALLBACK (windows_about), NULL);

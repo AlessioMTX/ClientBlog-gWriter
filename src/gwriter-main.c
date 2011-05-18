d5ea6616be9b908997bdccc0e38d23be245c1424
Patryk Rzucidlo <ptkdev@gmail.com>
2011-05-18 22:25:34 +0200

    Add IRC support: chat.freenode.net at #teamgwb

--- src/gwriter-main.c
+++ src/gwriter-main.c
@@ -1,25 +1,27 @@
 /* 
-*	gWriterBlog - Blog Editor For Linux Desktop
-*		Copyright (C) 2011  PTKDev
-*
-*		This program is free software: you can redistribute it and/or modify
-*		it under the terms of the GNU General Public License as published by
-*		the Free Software Foundation, either version 3 of the License, or
-*		(at your option) any later version.
-*
-*		This program is distributed in the hope that it will be useful,
-*		but WITHOUT ANY WARRANTY; without even the implied warranty of
-*		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*		GNU General Public License for more details.
-*
-*		You should have received a copy of the GNU General Public License
-*		along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*		
-*			
-*		Author: Patryk Rzucidlo (PTKDev)
-*		Twitter: @ptkdev / @gwriterblog_en
-*		WebSite: http://www.gwriterblog.org
-*/
+ *	gWriterBlog - Blog Editor For Linux Desktop
+ *		Copyright (C) 2011  PTKDev, Gaudo, RoxShannon
+ *
+ *		This program is free software: you can redistribute it and/or modify
+ *		it under the terms of the GNU General Public License as published by
+ *		the Free Software Foundation, either version 3 of the License, or
+ *		(at your option) any later version.
+ *
+ *		This program is distributed in the hope that it will be useful,
+ *		but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *		GNU General Public License for more details.
+ *
+ *		You should have received a copy of the GNU General Public License
+ *		along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *		
+ *			
+ *		Author: Patryk Rzucidlo (PTKDev)
+ *		Twitter: @ptkdev / @gwriterblog_en
+ *		WebSite: http://www.gwriterblog.org
+ * 		IRC: chat.freenode.net at #teamgwb
+ */
+ 
 // SuBMenu File
   /* Fix Icons Linux
    * gconftool-2 --type boolean --set /desktop/gnome/interface/buttons_have_icons true
